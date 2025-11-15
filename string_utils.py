d677d9d95df6641f70104a7a99633ad4bbf1fcbd
jawadgh03 <jawadghaname12@gmail.com>
2025-11-15 18:33:30 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -14,17 +14,16 @@ def split_before_each_uppercases(formula):
     return parts
 
 def split_at_first_digit(formula):
-       digit_location = 1
-
-     for ch in formula[1:]:
-         if ch.isdigit():
+    digit_location = 1
+        for ch in formula[1:]:
+            if ch.isdigit():
             break
-        digit_location += 1
+       digit_location += 1
 
     if digit_location == len(formula):
         return formula, 1
 
-       prefix = formula[:digit_location]
-       number = int(formula[digit_location:])
+    prefix = formula[:digit_location]
+    number = int(formula[digit_location:])
 
     return prefix, number
